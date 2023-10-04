78e15bf2ecfa9cde2beaf285dfdde7157849c73a
Roberto Ughi <umbe77@gmail.com>
2023-10-04 22:56:40 +0200

    Initial commit non tanto initial

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,131 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{ 'kyazdani42/nvim-web-devicons' },
+	{ 'ryanoasis/vim-devicons' },
+	{
+		-- LSP Configuration & Plugins
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			{ 'williamboman/mason.nvim', config = true },
+			'williamboman/mason-lspconfig.nvim',
+
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			'folke/neodev.nvim',
+		},
+	},
+
+	{
+		-- Autocompletion
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+
+			-- Adds LSP completion capabilities
+			'hrsh7th/cmp-nvim-lsp',
+
+			-- Adds a number of user-friendly snippets
+			'rafamadriz/friendly-snippets',
+		},
+	},
+	{ 'kosayoda/nvim-lightbulb' },
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			'nvim-lua/plenary.nvim'
+		}
+	},
+	{
+		-- Theme inspired by Atom
+		'navarasu/onedark.nvim',
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme 'onedark'
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		config = function()
+			require('todo-comments').setup({})
+		end,
+	},
+	{
+		-- Set lualine as statusline
+		'nvim-lualine/lualine.nvim',
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = false,
+				theme = 'onedark',
+				component_separators = '|',
+				section_separators = '',
+			},
+		},
+	},
+	{
+		-- Add indentation guides even on blank lines
+		'lukas-reineke/indent-blankline.nvim',
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help indent_blankline.txt`
+		main = "ibl",
+		opts = {},
+	},
+	-- "gc" to comment visual regions/lines
+	{ 'numToStr/Comment.nvim',  opts = {} },
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+			-- Only load if `make` is available. Make sure you have the system
+			-- requirements installed.
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				build = 'make',
+				cond = function()
+					return vim.fn.executable 'make' == 1
+				end,
+			},
+		},
+	},
+	{
+		-- Highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ':TSUpdate',
+	},
+	{
+		'nvimdev/lspsaga.nvim',
+		config = function()
+			require('lspsaga').setup({})
+		end,
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter',
+			'nvim-tree/nvim-web-devicons',
+		}
+	},
+}, {})
---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,37 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.title = true
+vim.o.fileencoding = "utf-8"
+vim.o.mouse = "a"
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.hlsearch = false
+
+vim.o.undofile = true
+vim.o.clipboard = "unnamedplus"
+vim.o.showtabline = false
+vim.o.showmode = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
+vim.o.cmdheight = 2
+
+vim.bo.smartindent = true
+vim.bo.expandtab = true
+vim.bo.tabstop = 4
+vim.bo.shitwidth = 4
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.wrap = true
+vim.wo.cursorline = true
+vim.wo.signcolumn = "yes"
+
